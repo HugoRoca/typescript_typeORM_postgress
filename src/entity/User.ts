b811c3ec8ca9aaf1c6e726c9f94b779432077463
hugoroca <hugo.rock20@hotmail.com>
2020-05-31 22:31:54 -0500

    add timestamps

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,29 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    firstname: string
+  @Column()
+  firstname: string;
 
-    @Column()
-    lastname: string
+  @Column()
+  lastname: string;
 
-    @Column({ nullable: true})
-    email: string
+  @Column({ nullable: true })
+  email: string;
+
+  @CreateDateColumn({ type: "timestamp", nullable: true })
+  createdAt: Date;
+
+  //@Column("timestamp", { precision: 3, default: () => "CURRENT_TIMESTAMP(3)", onUpdate: "CURRENT_TIMESTAMP(3)"})
+  @UpdateDateColumn({ type: "timestamp", nullable: true })
+  updatedAt: Date;
 }
